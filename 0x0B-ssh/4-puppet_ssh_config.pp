361e2ee9413ea5fe9977e54bc33e327fbc129d4e
afarizap <afarizap@gmail.com>
2020-12-08 19:00:24 +0000

    puppet to config

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,3 +1,5 @@
+# set up your client SSH configuration file so that you can connect to a server
+# without typing a password.
 file { 'Turn off passwd auth':
     path => '/etc/ssh/ssh_config',
     content => 'BachMode yes',
