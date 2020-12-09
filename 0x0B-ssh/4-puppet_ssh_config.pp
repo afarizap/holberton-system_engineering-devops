2bdd3d9b36bcba76d2c74ae088c76fce85135e4d
afarizap <afarizap@gmail.com>
2020-12-09 02:28:43 +0000

    asd

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,10 +1,10 @@
 # set up your client SSH configuration file so that you can connect to a server
 # without typing a password.
-File_line { 'Turn off passwd auth':
+file_line { 'Turn off passwd auth':
   path   => '/etc/ssh/ssh_config',
   line   => 'BachMode yes',
 }
-File_line { 'Declare identity file':
+file_line { 'Declare identity file':
   path   => '/etc/ssh/ssh_config',
   line   => 'IdentityFile ~/.ssh/holberton',
 }
