43b73ad153e79fcc1cc26c34f0b94e4b9b922fa7
afarizap <afarizap@gmail.com>
2021-02-01 13:53:04 -0500

    add task 0 making requests from api using path and query, then formating output with the needed info

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" using this REST API, for a given employee ID, returns information about his/her """
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    host = "https://jsonplaceholder.typicode.com/"
+    path = "users/" + sys.argv[1]
+
+    json_response = requests.get(host + path).json()
+    print("Employee {} is done with tasks".format(json_response.get('name')), end="")
+
+    query = "todos?userId=" + sys.argv[1]
+    json_response = requests.get(host + query).json()
+
+    done_tasks = []
+    for task in json_response:
+        if task.get("completed") is True:
+            done_tasks.append(task)
+
+    print("({}/{}):".format(len(done_tasks), len(json_response)))
+    for task in done_tasks:
+        print("\x09\x20{}".format(task.get("title")))
