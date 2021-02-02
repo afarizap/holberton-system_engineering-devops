563bc9a370e9dea3b03f8f97ba25e97d117d15d3
afarizap <afarizap@gmail.com>
2021-02-01 21:02:50 -0500

    add cvs and using quotechar and quote all to format in a new file

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" export data in the CSV format """
+import csv
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    host = "https://jsonplaceholder.typicode.com/"
+    path = "users/" + sys.argv[1]
+
+    username = requests.get(host + path).json().get("username")
+
+    query = "todos?userId=" + sys.argv[1]
+    tasks = requests.get(host + query).json()
+
+    listed_items = []
+    for item in tasks:
+        listed_items.append(
+            [sys.argv[1],
+             username,
+             item.get("completed"),
+             item.get("title")]
+        )
+
+    with open("USER_ID.csv", "w") as f:
+        writer = csv.writer(f, quotechar="\"", quoting=csv.QUOTE_ALL)
+        for row in listed_items:
+            writer.writerow(row)
